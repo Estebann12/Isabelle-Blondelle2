7bf8e1c8cdd0176052fc32a5860a249a9f0693c8
Estebann <estebannkrebs2@gmail.com>
2024-05-28 20:58:52 +0200

    contact+photo

--- contact.php
+++ contact.php
@@ -1,12 +1,12 @@
 <?php
 if (isset($_POST["email"])) {
     $message = "Ce message vous a été envoyé via votre site Web
-    Nom : " . $_POST["nom"] . "
+    Nom : " . $_POST["name"] . "
     Email : " . $_POST["email"] . "
     Message : " . $_POST["message"] . "
     Numéro : " . $_POST["phone"];
 
-    $retour = mail("estebannkrebs2@gmail.com", $_POST["objet"], $message, "From:isabelleblondellesite@gmail.com\r\nReply-to:" . $_POST["email"]);
+    $retour = mail("estebannkrebs2@gmail.com", $_POST["objet"], $message, "From:estebann.lana@gmail.com\r\nReply-to:" . $_POST["email"]);
     if ($retour) {
         echo "<p>L'email à bien été envoyé</p>";
     }
